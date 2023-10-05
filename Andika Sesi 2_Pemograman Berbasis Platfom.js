7bcdbdde163fb66fea4df725c011784e07917dce
universepy99 <114472231+universepy99@users.noreply.github.com>
2023-10-05 22:17:16 +0700

    Add files via upload

---
+++ Andika Sesi 2_Pemograman Berbasis Platfom.js
@@ -0,0 +1,13 @@
+var fullName = "Andika Rizky D Sambo Harahap"
+var lastName = "Harahap"
+var asalKampus = "Nusa Putra"
+var asalProdi = "Program Studi Teknik Informatika"
+var age = 19;
+var isMarried = false;
+
+console.log(fullName);
+console.log(lastName);
+console.log(asalKampus);
+console.log(asalProdi);
+console.log ("age:",age);
+console.log("isMarried:", isMarried);
